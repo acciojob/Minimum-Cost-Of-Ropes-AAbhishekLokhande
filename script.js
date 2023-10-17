fe37f2ebfdd440d5c5e806569df071c4f22ef35e
Abhishek Lokhande <115551272+AAbhishekLokhande@users.noreply.github.com>
2023-10-17 17:36:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 function calculateMinCost() {
   //your code here
-  
+    const input = document.getElementById("rope-lengths").value;
+    const lengths = input.split(",").map(item => parseInt(item.trim()));
+
+    const result = document.getElementById("result");
+    result.innerHTML = minCost(lengths);
+}
+
+function minCost(lengths) {
+    if (lengths.length < 2) {
+        return 0;
+    }
+
+    // Sorting the array initially
+    lengths.sort((a, b) => a - b);
+
+    let totalCost = 0;
+    let currentCost = 0;
+
+    // Repeatedly connecting the smallest ropes
+    while (lengths.length > 1) {
+        currentCost = lengths[0] + lengths[1];
+        totalCost += currentCost;
+        // Removing the two smallest ropes and adding their combined length
+        lengths.splice(0, 2, currentCost);
+        // Sorting the array again to maintain the order
+        lengths.sort((a, b) => a - b);
+    }
+
+    return totalCost;
   
   
 }  
